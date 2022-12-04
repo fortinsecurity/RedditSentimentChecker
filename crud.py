48bd15b0acfc96d8d69aa0b4e2d556b591db775b
Inky Zima <inky.zima@gmail.com>
2022-12-04 21:09:19 +0100

    working sqlite db and POST and GET for sentiment

---
+++ crud.py
@@ -0,0 +1,48 @@
+from sqlalchemy.orm import Session
+
+import models, schemas
+import random
+
+
+def get_user(db: Session, user_id: int):
+    return db.query(models.User).filter(models.User.id == user_id).first()
+
+
+def get_user_by_email(db: Session, email: str):
+    return db.query(models.User).filter(models.User.email == email).first()
+
+
+def get_users(db: Session, skip: int = 0, limit: int = 100):
+    return db.query(models.User).offset(skip).limit(limit).all()
+
+
+def create_user(db: Session, user: schemas.UserCreate):
+    fake_hashed_password = user.password + "notreallyhashed"
+    db_user = models.User(email=user.email, hashed_password=fake_hashed_password)
+    db.add(db_user)
+    db.commit()
+    db.refresh(db_user)
+    return db_user
+
+
+def get_items(db: Session, skip: int = 0, limit: int = 100):
+    return db.query(models.Item).offset(skip).limit(limit).all()
+
+
+def create_user_item(db: Session, item: schemas.ItemCreate, user_id: int):
+    db_item = models.Item(**item.dict(), owner_id=user_id)
+    db.add(db_item)
+    db.commit()
+    db.refresh(db_item)
+    return db_item
+
+
+def get_sentiment_by_subreddit_and_topic(db: Session, subreddit: str, topic: str):
+    return db.query(models.Sentiment).filter(models.Sentiment.subreddit == subreddit, models.Sentiment.topic == topic).first()
+
+def createSentiment(db: Session, sentiment: schemas.SentimentCreate):
+        db_sentiment = models.Sentiment(subreddit="crypto", topic="eth",sentimentrating=random.randint(1,10))
+        db.add(db_sentiment)
+        db.commit()
+        db.refresh(db_sentiment)
+        return db_sentiment
--- main.py
+++ main.py
@@ -1,19 +1,21 @@
-from typing import Union
 
-from fastapi import FastAPI
-from pydantic import BaseModel
+
+from fastapi import Depends, FastAPI, HTTPException
+from sqlalchemy.orm import Session
+
+import crud, models, schemas
+from database import SessionLocal, engine
+
+models.Base.metadata.create_all(bind=engine)
 
 app = FastAPI()
 
-
-class Item(BaseModel):
-    name: str
-    price: float
-    is_offer: Union[bool, None] = None
-
-class SentimentRequest(BaseModel):
-    subreddit: str
-    topic: str
+def get_db():
+    db = SessionLocal()
+    try:
+        yield db
+    finally:
+        db.close()
 
 
 @app.get("/")
@@ -21,14 +23,14 @@ def read_root():
     return {"Hello": "World"}
 
 
-@app.get("/items/{item_id}")
+""" @app.get("/items/{item_id}")
 def read_item(item_id: int, q: Union[str, None] = None):
     return {"item_id": item_id, "q": q}
+ """
 
-
-@app.put("/items/{item_id}")
+""" @app.put("/items/{item_id}")
 def update_item(item_id: int, item: Item):
-    return {"item_name": item.name, "item_id": item_id}
+    return {"item_name": item.name, "item_id": item_id} """
 
 '''
 query params example
@@ -37,11 +39,47 @@ query params example
 fake_items_db = [{"item_name": "Foo"}, {"item_name": "Bar"}, {"item_name": "Baz"}]
 
 
-@app.get("/items/")
+""" @app.get("/items/")
 async def read_item(skip: int = 0, limit: int = 10):
-    return fake_items_db[skip : skip + limit]
+    return fake_items_db[skip : skip + limit] """
+
+
+@app.post("/users/", response_model=schemas.User)
+def create_user(user: schemas.UserCreate, db: Session = Depends(get_db)):
+    db_user = crud.get_user_by_email(db, email=user.email)
+    if db_user:
+        raise HTTPException(status_code=400, detail="Email already registered")
+    return crud.create_user(db=db, user=user)
+
+
+@app.get("/users/", response_model=list[schemas.User])
+def read_users(skip: int = 0, limit: int = 100, db: Session = Depends(get_db)):
+    users = crud.get_users(db, skip=skip, limit=limit)
+    return users
+
+
+@app.get("/users/{user_id}", response_model=schemas.User)
+def read_user(user_id: int, db: Session = Depends(get_db)):
+    db_user = crud.get_user(db, user_id=user_id)
+    if db_user is None:
+        raise HTTPException(status_code=404, detail="User not found")
+    return db_user
+
+
+@app.post("/users/{user_id}/items/", response_model=schemas.Item)
+def create_item_for_user(
+    user_id: int, item: schemas.ItemCreate, db: Session = Depends(get_db)
+):
+    return crud.create_user_item(db=db, item=item, user_id=user_id)
+
+
+@app.get("/items/", response_model=list[schemas.Item])
+def read_items(skip: int = 0, limit: int = 100, db: Session = Depends(get_db)):
+    items = crud.get_items(db, skip=skip, limit=limit)
+    return items
 
 '''
+
 GET /sentiment?subreddit=crypto&topic=etherium
 '''
 
@@ -51,8 +89,20 @@ fake_sentiment_db = [{
     "sentiment":"8"
     }]
 
-@app.get("/sentiment/")
+""" @app.get("/sentiment/")
 def getSentiment(subreddit: str, topic: str):
-    return fake_sentiment_db[0]
+    return fake_sentiment_db[0] """
+
+@app.get("/sentiment/", response_model=schemas.Sentiment)
+def read_Sentiment(subreddit: str, topic: str, db: Session = Depends(get_db)):
+    sentiment = crud.get_sentiment_by_subreddit_and_topic(db, subreddit=subreddit, topic=topic)
+    return sentiment
+
+@app.post("/sentiment/", response_model=schemas.Sentiment)
+def create_Sentiment(sentiment:schemas.SentimentCreate, db: Session = Depends(get_db)):
+    db_sentiment = crud.get_sentiment_by_subreddit_and_topic(db, subreddit=sentiment.subreddit, topic=sentiment.topic)
+    if db_sentiment:
+        raise HTTPException(status_code=400, detail="Sentiment already exists")
+    return crud.createSentiment(db=db, sentiment=sentiment)
 
 # to run: uvicorn main:app --reload
